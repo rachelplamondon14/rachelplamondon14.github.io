f79cfde63329ba1fd19b37193be3e8bb459a37e2
Rachel Plamondon <rachel.plamondon14@gmail.com>
2021-02-24 20:41:33 -0500

    Smooth scrolling

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  //Smooth anchor scrolling
+  $('a[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+    return false;
+  });
+  
+});
